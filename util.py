80301d03f6eecd09660f98e99e9a10f88f4ac7a7
FDSM_lhn <FDSM_lhn@yahoo.com>
2017-05-31 11:31:41 +0800

    != None changes

--- util.py
+++ util.py
@@ -3,7 +3,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 import os
 import re
 
-word_lower_bound = 3
+word_lower_bound = 1
 
 def array2list(corpus):
 	D,W= corpus.shape
@@ -24,13 +24,29 @@ def path2corpus(path):
 	vectorizer = CountVectorizer(input = 'filename',lowercase = True,stop_words = 'english')
 	dtm = vectorizer.fit_transform(filenames).toarray()
 	vocab = np.array(vectorizer.get_feature_names())
+	title  = np.array([re.sub(".txt$","", txt) for txt in txt_list])
 
-	#delete other stopwords as well! numbers, individual characters, and characters appears less than x document.
-	abandon_index_1 = np.where(dtm.sum(axis = 0)<=word_lower_bound)[0]
-	abandon_index_2 = np.where([re.search(r'(^[a-zA-Z]$)|([0-9])',v) == None for v in vocab])[0]
-	abandon_index = np.union1d(abandon_index_1,abandon_index_2)
-
+	#delete other stopwords as well! numbers, individual characters, and 
+	#characters appears less than x document will be convert to a specific token
+	abandon_index = np.where([re.search(r'(^[a-zA-Z]$)|([0-9])',v) != None for v in vocab])[0]
 	dtm_n= np.delete(dtm, abandon_index,axis=1)
 	vocab_n = np.delete(vocab, abandon_index,axis=0)
-	title  = np.array([re.sub(".txt$","", txt) for txt in txt_list])
-	return dtm_n, title, vocab_n
+
+	spec_list = []
+	for col in range(dtm_n.shape[1]):
+		if len(np.nonzero(dtm_n[:,col])[0]) <=word_lower_bound:
+			spec_list.append(col)
+
+	if spec_list:
+		temp = dtm_n[:,spec_list]
+		dtm_sp = np.delete(dtm_n, spec_list,axis=1)
+		dtm_sp = np.append(dtm_sp, temp.sum(1)[np.newaxis].T,axis=1)
+
+		vocab_sp = np.delete(vocab_n, spec_list,axis=0)
+		vocab_sp = np.append(vocab_sp, '0s_token0')
+		return dtm_sp,title,vocab_sp
+	else:
+		return dtm_n,title,vocab_n
+
+
+def csv2corpus(doc_list)
